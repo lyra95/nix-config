c5a7cb5defe61d36cf13ad40b3294752e3d2049c
jo <95hyouka@gmail.com>
2024-09-21 23:15:55 +0900

    git: add some options

--- homeManagerModules/git/default.nix
+++ homeManagerModules/git/default.nix
@@ -11,10 +11,38 @@ in {
   };
 
   config = lib.mkIf config.git.enable {
+    # todo: add gpg signing
     programs.git = {
       enable = true;
       userName = "jo";
       userEmail = "95hyouka@gmail.com";
+
+      aliases = {
+        # Prettier `git log`
+        lg = "log --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        rb = "rebase --interactive";
+        last = "show HEAD";
+        oops = "commit -a --amend --no-edit";
+        fp = "push --force";
+        ig = "!vim \"$(git rev-parse --show-toplevel)/.gitignore\"";
+        gc-all = "gc -q --prune --aggressive --keep-largest-pack --force";
+      };
+
+      delta = {
+        enable = true;
+        options = {
+          navigate = true;
+          line-numbers = true;
+          hyperlinks = true;
+          side-by-side = true;
+          colorMoved = "default";
+        };
+      };
+
+      extraConfig = {
+        init.defaultBranch = "main";
+        merge.conflictstyle = "diff3";
+      };
     };
 
     age.secrets.github_ed25519 = lib.mkIf ageEnabled {
