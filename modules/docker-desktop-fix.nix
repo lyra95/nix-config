3fca13f1291188bdeb9790968ad0b430e7b393ed
95hyouka <95hyouka@gmail.com>
2024-09-26 10:28:38 +0000

    chore: resolve some warnings

--- modules/docker-desktop-fix.nix
+++ modules/docker-desktop-fix.nix
@@ -3,17 +3,16 @@
   lib,
   pkgs,
   ...
-}:
-with lib; {
-  options.fix.docker-desktop.enable = mkEnableOption "docker desktop fix";
+}: {
+  options.fix.docker-desktop.enable = lib.mkEnableOption "docker desktop fix";
 
   config = let
     resources = "C:\\Program Files\\Docker\\Docker\\resources";
   in
-    mkIf (config.wsl.docker-desktop.enable && config.fix.docker-desktop.enable) {
+    lib.mkIf (config.wsl.docker-desktop.enable && config.fix.docker-desktop.enable) {
       systemd.services.docker-desktop-proxy = {
         # This Command Line Interface is keep changing. This is the latest command to run the docker-desktop-proxy (Docker Desktop Version: 4.33.1 (161083))
-        script = mkForce ''
+        script = lib.mkForce ''
           ${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop '${resources}'
         '';
         path = [pkgs.mount];
