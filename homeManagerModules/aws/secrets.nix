f975895d6313de1a94e77fd12f4b308dd186fabf
95hyouka <95hyouka@gmail.com>
2024-09-26 12:27:40 +0000

    rekey secrets

--- homeManagerModules/aws/secrets.nix
+++ homeManagerModules/aws/secrets.nix
@@ -1,5 +1,6 @@
 let
+  home = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDnXUyIHoD17qpH4m8aRfCDXNqYV27sTSlttEx3/ge/M root@nixos";
   work = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAsTK5qze+z5HT/RGoYKanltHGUm+ed8RrZSUeD8XIU3 nixos@nixos";
 in {
-  "credentials.age".publicKeys = [work];
+  "credentials.age".publicKeys = [work home];
 }
