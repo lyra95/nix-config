25b875d53280c73bdd9df483341724541a57e231
jo <95hyouka@gmail.com>
2024-09-28 15:22:27 +0000

    move nixvim setting into other flake

--- flake.nix
+++ flake.nix
@@ -3,26 +3,38 @@
 
   inputs = {
     systems.url = "github:nix-systems/default";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.systems.follows = "systems";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
       inputs.systems.follows = "systems";
     };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim.url = "github:nix-community/nixvim/nixos-24.05";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+    nvim = {
+      url = "github:lyra95/nvim/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
 
   outputs = {
@@ -31,22 +43,13 @@
     flake-utils,
     agenix,
     home-manager,
-    nixvim,
+    nvim,
     ...
   }: let
     # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/8
     # import vs legacyPackages
     # the latter has performance gain
     pkgsBuilder = system: nixpkgs.legacyPackages.${system};
-    nixvimBuilder = system: let
-      nixvim' = nixvim.legacyPackages.${system};
-      nixvimModule = {
-        pkgs = pkgsBuilder system;
-        module = import ./nixvim;
-        extraSpecialArgs = {};
-      };
-    in
-      nixvim'.makeNixvimWithModule nixvimModule;
   in
     {
       nixosConfigurations = {
@@ -74,7 +77,7 @@
                 ];
                 extraSpecialArgs = {};
                 users."jo" = (import ./home.nix) {
-                  nixvim = nixvimBuilder "x86_64-linux";
+                  vim = nvim.packages."x86_64-linux";
                 };
               };
             }
@@ -84,9 +87,8 @@
     }
     // flake-utils.lib.eachDefaultSystem (system: let
       pkgs = pkgsBuilder system;
-      nvim = nixvimBuilder system;
     in {
       formatter = pkgs.alejandra;
-      devShells.default = pkgs.mkShell {packages = [nvim];};
+      devShells.default = pkgs.mkShell {packages = [nvim.packages."${system}".default];};
     });
 }
