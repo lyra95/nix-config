a1434fed0575a8e9ad3f3b4f808dc8d61999f350
jo <95hyouka@gmail.com>
2024-10-21 01:27:56 +0900

    name changed

--- modules/nixos/xrdp/default.nix
+++ modules/nixos/xrdp/default.nix
@@ -13,13 +13,13 @@
     services.xrdp.openFirewall = true;
 
     # Enable the KDE Plasma Desktop Environment.
-    services.xserver.displayManager.sddm.enable = true;
+    services.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
 
     # Configure keymap in X11
-    services.xserver = {
+    services.xserver.xkb = {
       layout = "us";
-      xkbVariant = "";
+      variant = "";
     };
   };
 }
