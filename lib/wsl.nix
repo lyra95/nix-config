8b66760a270cb6fee862942d49cab537bf225047
jo <95hyouka@gmail.com>
2024-11-02 07:37:13 +0000

    write `open` command on wsl

--- lib/wsl.nix
+++ lib/wsl.nix
@@ -29,6 +29,35 @@ inputs @ {
       wsl.enable = true;
       wsl.defaultUser = userName;
       wsl.wslConf.network.hostname = name;
+
+      programs.bash.interactiveShellInit = ''
+        wsl_to_windows_path() {
+                local wsl_path
+                wsl_path=$(readlink -f "$1")
+
+                local win_path
+
+                if [[ "$wsl_path" =~ ^/mnt/([a-z])(|/.*) ]]; then
+                        # Convert /mnt/c/... to C:\...
+                        local drive="''${BASH_REMATCH[1]}"
+                        local path="''${BASH_REMATCH[2]}"
+                        path="''${path//\//\\}"
+                        win_path="''${drive^^}:''${path:-\\}"
+                else
+                        # Convert /home/user/... to \\wsl$\distro_name\...
+                        local distro_name=NixOS
+                        win_path="\\\\wsl.localhost\\''${distro_name}\\''${wsl_path//\//\\}"
+                fi
+
+                echo "$win_path"
+        }
+
+        function open {
+                local path
+                path=$(wsl_to_windows_path "$1")
+                explorer.exe /root="$path"
+        }
+      '';
     };
   };
 in
