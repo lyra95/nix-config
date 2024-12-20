9f5106714da676ed00c7793b6e08e79c14df6123
jo <95hyouka@gmail.com>
2024-12-20 13:50:49 +0900

    asdf

--- identities.nix
+++ identities.nix
@@ -4,4 +4,5 @@
   dell = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJSlCdDLrVIKwowSvVEeF2cL5j5og2A/UahY7C/BaWM jo@dell";
   nas-root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGdHb/Du7guROKocWc0zKre58igNU5M/IBmqIoQNZq3Q root@nixos";
   nas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPf+yTr/wN+xdQFsUl8iZu5mY1sni7laQRPAITFRCqiF jo@jonuc";
+  macbook-air = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBd8ELl04FtLdBLCK6AG4iLbPX2ae5V7NZGxrdgiJ2dq jo@jo-macbook-air.local";
 }
