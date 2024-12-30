5c4b0d914f54d415a46f342907c4e6f3473738ea
95hyouka <95hyouka@gmail.com>
2024-12-30 10:03:15 +0000

    copy deleted feature from source

--- modules/wsl/docker-desktop-fix.nix
+++ modules/wsl/docker-desktop-fix.nix
@@ -10,12 +10,20 @@
     resources = "C:\\Program Files\\Docker\\Docker\\resources";
   in
     lib.mkIf (config.wsl.docker-desktop.enable && config.fix.docker-desktop.enable) {
+      # https://github.com/nix-community/NixOS-WSL/commit/3a22a1981ebb1edea051cf918a98fe10e1b3a99e
+      # this service unit does the same thing with 'Docker Desktop -> Settings -> Resources -> WSL Integration toggle'
+      # but docker-desktop-proxy commandline interface often makes breaking changes
       systemd.services.docker-desktop-proxy = {
-        # This Command Line Interface is keep changing. This is the latest command to run the docker-desktop-proxy (Docker Desktop Version: 4.33.1 (161083))
-        script = lib.mkForce ''
-          ${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop '${resources}'
-        '';
         path = [pkgs.mount];
+        description = "Docker Desktop proxy";
+        script = ''
+          ${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --distro-name NixOS --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "${resources}"
+        '';
+        wantedBy = ["multi-user.target"];
+        serviceConfig = {
+          Restart = "on-failure";
+          RestartSec = "30s";
+        };
       };
     };
 }
