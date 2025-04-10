29850e12ac4bbfa355790df6cf3871ec8b0ee2ca
95hyouka <95hyouka@gmail.com>
2025-04-10 04:54:31 +0000

    update to 24.11

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.systems.follows = "systems";
     };
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
@@ -24,7 +24,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
