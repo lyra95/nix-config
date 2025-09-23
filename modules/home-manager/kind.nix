7606b91bb77b70bdbb881e4b4da20a770e7e8afc
95hyouka <95hyouka@gmail.com>
2025-09-23 19:01:20 +0000

    refine kind script

--- modules/home-manager/kind.nix
+++ modules/home-manager/kind.nix
@@ -15,19 +15,22 @@
       };
 
       initExtra = ''
-        function enter-kind() {
-          ORIGINAL_IN_NIX_SHELL=$IN_NIX_SHELL
-          ORIGINAL_KUBECONFIG=$KUBECONFIG
+             function enter-kind() {
+               ORIGINAL_IN_NIX_SHELL=$IN_NIX_SHELL
+               ORIGINAL_KUBECONFIG=$KUBECONFIG
 
-          IN_NIX_SHELL=impure \
-            name=kind \
-            KUBECONFIG="~/.kube/kind-config" \
-            bash -c 'eval "$(kind completion bash)"; exec bash'
+        TEMPFILE=$(mktemp --suffix=.kubeconfig)
+               kind get kubeconfig > "$TEMPFILE"
 
-          IN_NIX_SHELL=$ORIGINAL_IN_NIX_SHELL
-          KUBECONFIG=$ORIGINAL_KUBECONFIG
-          unset ORIGINAL_IN_NIX_SHELL ORIGINAL_KUBECONFIG
-        }
+               IN_NIX_SHELL=impure \
+                 name=kind \
+                 KUBECONFIG="$TEMPFILE" \
+                 bash -c 'eval "$(kind completion bash)"; exec bash'
+
+               IN_NIX_SHELL=$ORIGINAL_IN_NIX_SHELL
+               KUBECONFIG=$ORIGINAL_KUBECONFIG
+               unset ORIGINAL_IN_NIX_SHELL ORIGINAL_KUBECONFIG
+             }
       '';
     };
   };
