32f5e71bc8639b082024bfb7025fc021283b3e1f
95hyouka <95hyouka@gmail.com>
2025-10-16 08:42:42 +0000

    asdf

--- modules/home-manager/kind.nix
+++ modules/home-manager/kind.nix
@@ -14,24 +14,7 @@
         ek = "enter-kind";
       };
 
-      initExtra = ''
-             function enter-kind() {
-               ORIGINAL_IN_NIX_SHELL=$IN_NIX_SHELL
-               ORIGINAL_KUBECONFIG=$KUBECONFIG
-
-        TEMPFILE=$(mktemp --suffix=.kubeconfig)
-               kind get kubeconfig > "$TEMPFILE"
-
-               IN_NIX_SHELL=impure \
-                 name=kind \
-                 KUBECONFIG="$TEMPFILE" \
-                 bash -c 'eval "$(kind completion bash)"; exec bash'
-
-               IN_NIX_SHELL=$ORIGINAL_IN_NIX_SHELL
-               KUBECONFIG=$ORIGINAL_KUBECONFIG
-               unset ORIGINAL_IN_NIX_SHELL ORIGINAL_KUBECONFIG
-             }
-      '';
+      initExtra = builtins.readFile ./enter-kind.sh;
     };
   };
 }
