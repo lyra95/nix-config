40ceaa58d14fbe9be32819de148737b5fb292e0e
95hyouka <95hyouka@gmail.com>
2025-10-16 09:05:00 +0000

    following deprecation guide while updating nixos

--- machines/nuc/generated/configuration.nix
+++ machines/nuc/generated/configuration.nix
@@ -24,7 +24,9 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # The option definition `sound' in `/nix/store/.../machines/nuc/generated/configuration.nix' no longer has any effect; please remove it.
+  # The option was heavily overloaded and can be removed from most configurations.
+  # sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
